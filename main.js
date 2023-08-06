e3cb461e7dff9699e056928a87b8fe33ad7142d8
anujt21 <anujdt21@gmail.com>
2023-08-05 19:07:17 -0700

    Simple edits

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 import * as THREE from 'three';
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer(
+	{
+	antialias:  true
+});
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild( renderer.domElement );
 
 // create a sphere
-const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+
+const material = new THREE.ShaderMaterial( { 
+	vertexShader,
+	fragmentShader
+} );
+
 const geometry = new THREE.SphereGeometry(5, 50, 50);
 const sphere = new THREE.Mesh( geometry, material);
 
