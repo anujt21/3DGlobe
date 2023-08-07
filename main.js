c11f3e1fa1fb4cf8585d70e254be6258fd1a9c0e
anujt21 <anujdt21@gmail.com>
2023-08-06 17:10:29 -0700

    Added clouds and stars

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ let targetRotationY = 0.01
 let mouseX = 0, mouseXOnMouseDown = 0, mouseY = 0, mouseYOnMouseDown = 0;
 const windowHalfX = window.innerWidth/2
 const windowHalfY = window.innerHeight/2
-const dragFactor = 0.00008
+const dragFactor = 0.0002
+const slowingFactor = 0.99
 
 // Create Scene and Renderer
 const scene = new THREE.Scene();
@@ -22,22 +23,38 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 document.body.appendChild( renderer.domElement );
 
-// create Earth geometry
+// create earth geometry
 const earthMaterial = new THREE.MeshPhongMaterial( { 
-	//wireframe: true,
 	map: new THREE.TextureLoader().load('textures/earthmap.jpeg'),
 	bumpMap: new THREE.TextureLoader().load('textures/earth_bump.jpg'),
 	bumpScale: 0.01
 } );
-const earthGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-
+const earthGeometry = new THREE.SphereGeometry(0.5, 64, 64);
 const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
 scene.add(earthMesh);
 
+// create cloud geometry
+const cloudMaterial = new THREE.MeshPhongMaterial( { 
+	map: new THREE.TextureLoader().load('textures/earthCloud.png'),
+	transparent: true
+} );
+const cloudGeometry = new THREE.SphereGeometry(0.505, 64, 64);
+const cloudMesh = new THREE.Mesh(cloudGeometry, cloudMaterial);
+scene.add(cloudMesh);
+
+// create start geometry
+const starGeometry = new THREE.SphereGeometry(50, 100, 100);
+const starMaterial = new THREE.MeshBasicMaterial({
+	map: new THREE.TextureLoader().load('textures/galaxy.png'),
+	side: THREE.BackSide
+});
+const starMesh =  new THREE.Mesh(starGeometry, starMaterial);
+scene.add(starMesh)
+
 // Add light
-const ambientlight = new THREE.AmbientLight(0xffffff, 0.2);
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.14);
 scene.add(ambientlight);
-const pointerlight = new THREE.PointLight(0xffffff, 40);
+const pointerlight = new THREE.PointLight(0xffffff, 30);
 pointerlight.position.set(2.5,1.5,2.5);
 scene.add(pointerlight);
 
@@ -71,6 +88,10 @@ function animate() {
 	requestAnimationFrame( animate );
 	earthMesh.rotateOnWorldAxis(new THREE.Vector3(0,1,0), targetRotationX);
 	earthMesh.rotateOnWorldAxis(new THREE.Vector3(1,0,0), targetRotationY);
+	cloudMesh.rotateOnWorldAxis(new THREE.Vector3(0,1,0), targetRotationX);
+	cloudMesh.rotateOnWorldAxis(new THREE.Vector3(1,0,0), targetRotationY);
+	targetRotationX *= slowingFactor;
+	targetRotationY *= slowingFactor;
 	renderer.render( scene, camera );
 }
 
